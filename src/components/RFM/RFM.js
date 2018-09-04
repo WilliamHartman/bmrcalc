6dc33d1c4bad019fa4456f456b58418f116ac9bc
William Hartman <hartman.william@gmail.com>
2018-09-04 16:30:45 -0700

    hooked RFM up

--- src/components/RFM/RFM.js
+++ src/components/RFM/RFM.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './RFM.css';
+import { connect } from 'react-redux';
+import { updateUserData, updateCurrentPage } from './../../redux/reducer';
+import { withRouter } from 'react-router';
 
-export default function RFM(){
-    return(
-        <div className="RFM">
-            RFM
-        </div>
-    );
+class RFM extends Component{
+    constructor(){
+        super();
+        
+        this.state = {
+
+        }
+      }
+    
+    render() {
+        return (
+            <div className="RFM">
+                RFM
+            </div>
+        );
+    }
+    
 }
+
+function mapStateToProps(state){
+    return {
+        user: state.user
+    }
+  }
+  
+export default withRouter(connect(mapStateToProps, { updateUserData, updateCurrentPage })(RFM));
